c980be658f7c7c1f69d2d86508665cd33f8c7425
joudy-yassin <joudyyassinjoudy@gmail.com>
2025-05-21 14:32:54 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,11 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/682783fbe9a0a8190cd23d47/1ird5n414';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
